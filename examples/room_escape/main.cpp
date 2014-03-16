0d2f0712effa35caab1a8b479e72ca8dfd19183a
Andy Huang <andyspider@126.com>
2014-03-16 18:13:03 +0800

    实现了一个来自Q-learning的例子:room_escape

---
+++ examples/room_escape/main.cpp
@@ -0,0 +1,43 @@
+// -----------------------------------------------------------------------------
+//
+// GIMCS -- Generalized Intelligence Model and Computer Simulation
+//
+// Copyright (C) 2013-2014, Andy Huang  <andyspider@126.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// -----------------------------------------------------------------------------
+
+
+#include <stdio.h>
+#include "gimcs/CSOSAgent.h"
+#include "gimcs/Mysql.h"
+#include "Escapee.h"
+#include "gimcs/DotViewer.h"
+
+int main(void)
+{
+    CSOSAgent agent(1, 0.9, 0.01);
+
+    Escapee escapee("Escapee", 2, 1000);
+    escapee.connectAgent(&agent);
+
+    escapee.launch();
+
+    // show states
+    DotViewer dv;
+    dv.attachStorage(&agent);
+    dv.show();
+
+    printf("spawn a new escapee with the learned agent.\n");
+    Escapee escapee2("Escapee2", 1, 10);
+    escapee2.setSps(2);
+    escapee2.connectAgent(&agent);
+    escapee2.launch();
+
+    return 0;
+}
+
+
